681ff8a5b17df83cee945e3c24174ced4713eb1b
anisha-singh-2004 <130084612+anisha-singh-2004@users.noreply.github.com>
2023-05-21 23:05:01 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,4 @@
+r=int(input("Input radius of the circle "))
+area= r**2*22/7
+print("Area of circle with radius",r)
+print("=",area)
---
+++ ext.py
@@ -0,0 +1,7 @@
+d={"py":'python',
+   "js":'javascript',
+   "c":'C',
+   'cc':"C++",
+   'java':"Java"}
+in=input("Input file name")
+d.get(in)
